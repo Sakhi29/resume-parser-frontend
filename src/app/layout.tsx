29d6cc80fc9fcc90dcec6a2d10a536416a76ab0a
Sakhi Rotliwala <rotliwalasakhi@gmail.com>
2024-09-03 22:34:58 +0530

    footer added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { NavBar } from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body>
         <NavBar />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <section className="bg-primary w-full h-[250px]">
+        
+      </section>
+    </div>
+  );
+}
+
+export default Footer;
